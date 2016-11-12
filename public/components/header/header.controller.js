c8f81db15b5055c84c14937035adffacac9edf4a
Brian Roth <brianroth5@gmail.com>
2016-11-12 13:24:43 -0500

    latest front end changes

--- public/components/header/header.controller.js
+++ public/components/header/header.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('cs3200project').controller('headerController', ['$scope', function($scope) {
+angular.module('cs3200project').controller('headerController', ['$scope', 'CONFIG', function($scope, CONFIG) {
   $scope.init = function() {
-
+    $scope.siteName = CONFIG.SITE_NAME;
   };
 
   $scope.init();
